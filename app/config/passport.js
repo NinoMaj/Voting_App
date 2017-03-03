24a6411a63f39f9f8f7fb426b08955b6abe2558c
NinoMaj <nino.majder@gmail.com>
2017-03-03 23:17:38 +0100

    google auth fix and font awsome fix v4

--- app/config/passport.js
+++ app/config/passport.js
@@ -55,9 +55,9 @@ module.exports = function (passport) {
 					return done(null, existingUser);
 				}
 				// profile.emails[0].value
-				User.findOne({ email: profile.email}, (err, existingEmailUser) => {
+				User.findOne({ email: profile._json.email}, (err, existingEmailUser) => {
 					if (err) { return done(err); }
-					if (existingEmailUser) {
+					if (null) {
 						// req.flash('errors', { msg: 'There is already a Google account that belongs to you. Sign in with that account or delete it, then link it with your current account.' });
 						console.log('There is already a Google account that belongs to you. Sign in with that account or delete it, then link it with your current account.')
 						done(err);
